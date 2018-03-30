ef74cee797de3d2f676c9a113b5d09b7f2b0819b
Matt <mattmillergraphics@gmail.com>
2018-03-30 16:20:52 +0100

    Adding a spring arm to have the camera correctly attached for 3rd person

--- Source/CoopHordeGame/Private/SCharacter.cpp
+++ Source/CoopHordeGame/Private/SCharacter.cpp
@@ -3,6 +3,7 @@
 #include "SCharacter.h"
 #include "../Public/SCharacter.h"
 #include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
 ASCharacter::ASCharacter()
@@ -10,8 +11,12 @@ ASCharacter::ASCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	SpringArmComp = CreateAbstractDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+	SpringArmComp->bUsePawnControlRotation = true;
+	SpringArmComp->SetupAttachment(RootComponent);
+
 	CameraComp = CreateAbstractDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
-	CameraComp->bUsePawnControlRotation = true;
+	CameraComp->SetupAttachment(SpringArmComp);
 }
 
 // Called when the game starts or when spawned
