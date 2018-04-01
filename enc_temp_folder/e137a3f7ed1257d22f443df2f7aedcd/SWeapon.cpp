5fd1f4969079424b6d1e9d19febf59c3dfc58139
Matt <mattmillergraphics@gmail.com>
2018-04-02 00:34:05 +0100

    Added debug consol var for weapon line trace

--- enc_temp_folder/e137a3f7ed1257d22f443df2f7aedcd/SWeapon.cpp
+++ enc_temp_folder/e137a3f7ed1257d22f443df2f7aedcd/SWeapon.cpp
@@ -8,9 +8,6 @@
 #include "Components/SkeletalMeshComponent.h"
 #include "Particles/ParticleSystemComponent.h"
 
-static int32 DebugWeaponDrawing = 0;
-FAutoConsoleVariableRef CVARDebugWeaponDrawing(TEXT("COOP.DebugWeapons"), DebugWeaponDrawing, TEXT("Draw debug lines for weapons"), ECVF_Cheat);
-
 // Sets default values
 ASWeapon::ASWeapon()
 {
@@ -67,10 +64,7 @@ void ASWeapon::Fire()
 			TracerEndPoint = Hit.ImpactPoint;
 		}
 
-		if (DebugWeaponDrawing > 0) 
-		{
-			DrawDebugLine(GetWorld(), EyeLocation, TraceEnd, FColor::Red, false, 1.0f, 0, 1.0f);
-		}
+		//DrawDebugLine(GetWorld(), EyeLocation, TraceEnd, FColor::Red, false, 1.0f, 0, 1.0f);
 
 		if (MuzzleEffect) 
 		{
