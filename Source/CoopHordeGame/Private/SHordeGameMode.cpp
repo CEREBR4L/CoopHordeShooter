67251e1873dd37f8500d3171ebd7174d1178904b
Matt <mattmillergraphics@gmail.com>
2018-04-21 21:02:08 +0100

    Wave based spawing of bots

--- Source/CoopHordeGame/Private/SHordeGameMode.cpp
+++ Source/CoopHordeGame/Private/SHordeGameMode.cpp
@@ -1,7 +1,54 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "SHordeGameMode.h"
+#include "TimerManager.h"
+
+ASHordeGameMode::ASHordeGameMode()
+{
+	WaveCount = 0;
+	TimeBetweenWaves = 15.f;
+}
+
+void ASHordeGameMode::StartWave()
+{
+	WaveCount++;
+
+	NumberOfBotsToSpawn = 2 * WaveCount;
+
+	GetWorldTimerManager().SetTimer(TimerHandle_BotSpawner, this, &ASHordeGameMode::SpawnBotTimerElapsed, 1.f, true, 0.f);
+}
 
 
+void ASHordeGameMode::EndWave()
+{
+	GetWorldTimerManager().ClearTimer(TimerHandle_BotSpawner);
+
+	PrepareForNextWave();
+}
 
 
+void ASHordeGameMode::StartPlay()
+{
+	Super::StartPlay();
+
+	PrepareForNextWave();
+}
+
+void ASHordeGameMode::PrepareForNextWave()
+{
+	FTimerHandle TimerHandle_NextWaveStart;
+	GetWorldTimerManager().SetTimer(TimerHandle_NextWaveStart, this, &ASHordeGameMode::StartWave, TimeBetweenWaves, false);
+}
+
+
+void ASHordeGameMode::SpawnBotTimerElapsed()
+{
+	SpawnNewBot();
+
+	NumberOfBotsToSpawn--;
+
+	if (NumberOfBotsToSpawn <= 0)
+	{
+		EndWave();
+	}
+}
--- Source/CoopHordeGame/Public/SHordeGameMode.h
+++ Source/CoopHordeGame/Public/SHordeGameMode.h
@@ -14,7 +14,31 @@ class COOPHORDEGAME_API ASHordeGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+protected: 
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "GameMode")
+	void SpawnNewBot();
 	
-	
-	
+	void SpawnBotTimerElapsed();
+
+	void StartWave();
+
+	void EndWave();
+
+	void PrepareForNextWave();
+
+	FTimerHandle TimerHandle_BotSpawner;
+
+	int32 NumberOfBotsToSpawn;
+
+	int32 WaveCount;
+
+	UPROPERTY(EditDefaultsOnly, Category = "GameMode")
+	float TimeBetweenWaves;
+
+public:
+
+	ASHordeGameMode();
+
+	virtual void StartPlay() override;
 };
